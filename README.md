91564a2553690fe8b3ed5013e32ba7e908b70a18
Prince Agarwal <princeagarwalofficial@gmail.com>
2026-05-26 14:24:40 +0530

    Initial upload of Tomato food delivery project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tomato---Food-Delivery-App
+A full stack food delivery web application built with React, Node.js, Express, and MongoDB. Users can browse food items, add to cart, place orders, and track order status, while admins can manage food items and orders through an admin dashboard.
---
+++ backend/server.js
@@ -0,0 +1,52 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import cors from "cors"
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+dotenv.config();
+connectDB();
+
+
+const app = express();
+app.use(express.json());
+
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+//api endpoint 
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
